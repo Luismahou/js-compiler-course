9f02b765e259c92f9551f7049bd72cfa3ee6d777
Luismahou <luismahou@gmail.com>
2012-05-21 00:13:03 +1000

    MUS to NOTE. Supported commands: 'par' and 'seq'.

---
+++ mus/mus-compiler.js
@@ -0,0 +1,55 @@
+var compile = function(expr) {
+    var result = compileAndTime(expr, 0);
+    return result[0];
+};
+var compileAndTime = function(expr, startTime) {
+    if (expr.tag === 'par') {
+        return compilePar(expr, startTime);
+    }
+    else if (expr.tag === 'seq') {
+        return compileSeq(expr, startTime);
+    }
+    else {
+        return [[{ 
+            tag: 'note', 
+            pitch: expr.pitch, 
+            start: startTime, 
+            dur: expr.dur 
+        }], startTime + expr.dur];
+    }
+};
+
+var compilePar = function(expr, startTime) {
+    var leftResult = compileAndTime(expr.left, startTime);
+    var rightResult = compileAndTime(expr.right, startTime);
+    var finalTime = Math.max(leftResult[1], rightResult[1]);
+    var finalResult = leftResult[0];
+    for (var i = 0; i < rightResult[0].length; i++) {
+        finalResult.push(rightResult[0][i]);
+    }
+    return [finalResult, finalTime];
+};
+var compileSeq = function(expr, startTime) {
+    var leftResult = compileAndTime(expr.left, startTime);
+    var rightResult = compileAndTime(expr.right, leftResult[1]);
+    var finalResult = leftResult[0];
+    for (var i = 0; i < rightResult[0].length; i++) {
+        finalResult.push(rightResult[0][i]);
+    }
+    return [finalResult, rightResult[1]];
+};
+
+
+var melody_mus = 
+    { tag: 'seq',
+      left: 
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'a4', dur: 250 },
+         right: { tag: 'note', pitch: 'b4', dur: 250 } },
+      right:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'c4', dur: 500 },
+         right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+
+console.log(melody_mus);
+console.log(compile(melody_mus));
